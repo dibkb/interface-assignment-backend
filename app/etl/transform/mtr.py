9b77fde844b3fac69c5cbac3b66f690b79b2b476
dibkb <dibas9110@gmail.com>
2024-08-28 23:57:52 +0530

    transformation log errors

--- app/etl/transform/mtr.py
+++ app/etl/transform/mtr.py
@@ -1,6 +1,15 @@
+from ...logs.log import log_error
 def transform_mtr_df(df):
-    #Todo log errors
-    df = df[df['TransactionType'] != 'Cancel']
-    df.loc[df["TransactionType"] == "Refund", "TransactionType"] = "Return"
-    df.loc[df["TransactionType"] == "FreeReplacement", "TransactionType"] = "Return"
-    return df
+    try:
+        df = df[df['TransactionType'] != 'Cancel']
+        df.loc[df["TransactionType"] == "Refund", "TransactionType"] = "Return"
+        df.loc[df["TransactionType"] == "FreeReplacement", "TransactionType"] = "Return"
+        return df
+    except KeyError as k:
+        # transform_mtr_df error
+        log_error(k, context="KeyError in transform_mtr_df: Missing 'TransactionType' column")
+        raise
+    except Exception as e:
+        # log any other exception
+        log_error(e, context="General Error in transform_mtr_df")
+        raise
--- app/etl/transform/payment.py
+++ app/etl/transform/payment.py
@@ -1,12 +1,22 @@
+from ...logs.log import log_error
 def transform_payment_df(df):
-    df = df[df['Type'] != 'Transfer']
-    df = df.rename(columns={'Type': 'PaymentType', 'Date/Time': 'PaymentDate'})
-    df['PaymentType'] = df['PaymentType'].replace({
-        'Ajdustment': 'Order',
-        'FBA Inventory Fee': 'Order',
-        'Fulfilment Fee Refund': 'Order',
-        'Service Fee': 'Order',
-        'Refund': 'Return'
-    })
-    df['TransactionType'] = 'Payment'
-    return df
+    try:
+        df = df[df['Type'] != 'Transfer']
+        df = df.rename(columns={'Type': 'PaymentType', 'Date/Time': 'PaymentDate'})
+        df['PaymentType'] = df['PaymentType'].replace({
+            'Ajdustment': 'Order',
+            'FBA Inventory Fee': 'Order',
+            'Fulfilment Fee Refund': 'Order',
+            'Service Fee': 'Order',
+            'Refund': 'Return'
+        })
+        df['TransactionType'] = 'Payment'
+        return df
+    except KeyError as k:
+        # transform_payment_df error
+        log_error(k, context="KeyError in transform_payment_df: Missing 'Type' or 'Date/Time' column")
+        raise
+    except Exception as e:
+        # other general errors
+        log_error(e, context="General Error in transform_payment_df")
+        raise
