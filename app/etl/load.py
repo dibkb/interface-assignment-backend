e6cbf17c4e40d7cd17850fadb4240fa356c13520
dibkb <dibas9110@gmail.com>
2024-08-28 23:43:31 +0530

    load error handling

--- app/etl/load.py
+++ app/etl/load.py
@@ -1,19 +1,23 @@
 import pandas as pd
 import io
+from  ..logs.log import log_error
 
 def read_file(file):
-    content = file.file.read()
-    if file.filename.endswith('.csv'):
-        return pd.read_csv(io.StringIO(content.decode('utf-8')))
-    elif file.filename.endswith(('.xls', '.xlsx')):
-        return pd.read_excel(io.BytesIO(content))
-    else:
-        # Todo log this in json file
-        raise ValueError(f"Unsupported file format: {file.filename}")
+    try:
+        content = file.file.read()
+        if file.filename.endswith('.csv'):
+            return pd.read_csv(io.StringIO(content.decode('utf-8')))
+        elif file.filename.endswith(('.xls', '.xlsx')):
+            return pd.read_excel(io.BytesIO(content))
+        else:
+            raise ValueError(f"Unsupported file format: {file.filename}")
+    except Exception as e:
+        log_error(e,context=f"Error while reading file: {file.filename}")   
 
 def preprocess_dataframe(df:pd.DataFrame) -> pd.DataFrame:
     try:
         df.columns = df.columns.str.title().str.replace(' ', '')
         return df.map(lambda x: x.strip() if isinstance(x, str) else x)
     except Exception as e:
+        log_error(e,context=f"Cannot strip values: {df.Name}") 
         raise ValueError(f"Cannot strip values: {df.Name}")
